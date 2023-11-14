131859a0ec8c055394bcb2b62af671e9995d6317
SERGIO-ANDRES-SACHEZ-OSORNO <124899111+SERGIO-ANDRES-SACHEZ-OSORNO@users.noreply.github.com>
2023-11-13 22:25:12 -0500

    avance en la pagina

--- web-pyme/src/App.js
+++ web-pyme/src/App.js
@@ -1,13 +1,38 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
-const Home =():Element => <h1>Home</h1>
-const SearchPage = ():Element => <h1>SearchPage</h1>
+import {Link, Route, Routes } from 'react-router-dom';
+const Home =():Element => <h1>Home</h1> //ok
+
+const SearchPage = ():Element =>{  //ok
+  const Tacos = '/Tacos'//ok
+  return(  //ok
+  <div>
+
+    <h1>SearchPage</h1>
+    <Link to={Tacos}>tacos</Link>
+  </div>
+
+  )
+  
+} 
+const Tacos = ():Element => <h1>Tacos</h1>  //ok
+
 function App() {
   return (
     <div className="App">
+      <header>
+        <h1>Inicio</h1>
+        <nav>
+          <ul>
+            <li><Link to='/searchPage'>Search Page</Link></li>
+            <li><Link to='/'>Home</Link></li>
+            <li><Link to='/'>Tacos</Link></li>
+          </ul>
+        </nav>
+      </header>
      <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/SearchPage' element={<SearchPage/>}/>
+      <Route path='/Tacos' element={<Tacos/>}/>
      </Routes>  
     </div>
   );
